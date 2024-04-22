c257b5286b3925c5a6eba7f6015112ae3ac754fe
dorcus-nanteza-30 <142330820+dorcus-nanteza-30@users.noreply.github.com>
2024-04-22 17:39:36 +0300

    Add files via upload

---
+++ database exam/question 2.sql
@@ -0,0 +1,39 @@
+--CREATE TABLE employees (
+  --id INT PRIMARY KEY,
+  --name VARCHAR(50),
+  --age INTEGER,
+  --address VARCHAR(50)
+--);
+
+--INSERT INTO employees(id, name, age, address)
+--VALUES (1001, 'Rohan', 26, 'Delhi'),
+       --(1002, 'Ankit', 30, 'Gurgaon'),
+       --(1003, 'Gaurav', 27, 'Mumbai'),
+       --(1004, 'Raja', 32, 'Nagpur');
+
+--SELECT * FROM employees;
+
+--SELECT * FROM employees WHERE id = 1004;
+
+--SELECT * FROM employees WHERE name LIKE 'R%';
+
+--SELECT id, age, name 
+--FROM employees 
+--ORDER BY age ASC;
+
+--SELECT id, age, name 
+--FROM employees 
+--ORDER BY age DESC;
+
+
+--SELECT DISTINCT address 
+--FROM employees;
+
+
+
+
+
+
+
+
+
---
+++ database exam/question3.sql
@@ -0,0 +1,8 @@
+A primary key is a column in a relational database table that's distinctive for each record.
+A foreign key is a column or group of columns in a relational database table that provides a link between data in two tables.
+Entities include the following;
+Tv_series.
+Episode.
+Transmission
+Actor
+Director
